2340f6d585063fbe662e4a1990b84c992099a271
Anthony Scatchell <scatchell@gmail.com>
2019-05-26 22:59:26 +0100

    Allowing configurable range of equation statistics

--- src/clj/math_assist/equation_statistics.clj
+++ src/clj/math_assist/equation_statistics.clj
@@ -1,23 +1,23 @@
 (ns math-assist.equation-statistics
   (:require [math-assist.equations :refer [config-from-equation]]))
 
-(defn category-of-number [number]
-  (let [difference-from-nearest-ten (- number (mod number 10))]
+(defn- category-of-number [number range]
+  (let [difference-from-nearest-ten (- number (mod number range))]
     {:start difference-from-nearest-ten
-     :end   (+ 10 difference-from-nearest-ten)})
+     :end   (+ range difference-from-nearest-ten)})
   )
 
-(defn- group-to-number-categories [equation]
+(defn- group-to-number-categories [range equation]
   (let [max-number-in-equation (:max (config-from-equation (:equation equation)))]
-    (category-of-number max-number-in-equation)))
+    (category-of-number max-number-in-equation range)))
 
 (defn- correctness-of-eqn-category [[number-category eqns-in-category]]
   (let [percentage-correct (/ (count (filter #(:correct %) eqns-in-category)) (count eqns-in-category))
         correctness (* 100 (with-precision 3 (bigdec percentage-correct)))]
     (merge number-category {:correctness correctness})))
 
-(defn correctness [equations]
-  (let [groups (group-by group-to-number-categories equations)]
+(defn correctness [{:keys [equations range] :or {range 10}}]
+  (let [groups (group-by (partial group-to-number-categories range) equations)]
     (map correctness-of-eqn-category groups)))
 
 
--- test/math_assist/equation_statistics_test.clj
+++ test/math_assist/equation_statistics_test.clj
@@ -4,31 +4,43 @@
 
 (deftest equation-statistics
   (testing "can determine correctness of 10-20 equations"
-    (is (= (correctness [{:equation    "10*10"
-                          :correct     true}
+    (is (= (correctness {:equations [{:equation "10*10"
+                                      :correct  true}
 
-                         {:equation    "13*13"
-                          :correct     true}
+                                     {:equation "13*13"
+                                      :correct  true}
 
-                         {:equation    "13*13"
-                          :correct     false}])
+                                     {:equation "13*14"
+                                      :correct  false}]
+                         :range     10})
            [{:start       10
              :end         20
              :correctness 66.7M}])))
 
-  (testing "categorize numbers"
-    (is (= (category-of-number 7) {:start 0 :end 10}))
-    (is (= (category-of-number 12) {:start 10 :end 20})))
+  (testing "can determine correctness of 10-15 equations"
+    (is (= (correctness {:equations [{:equation "10*10"
+                                      :correct  true}
+
+                                     {:equation "13*13"
+                                      :correct  true}
+
+                                     {:equation "13*14"
+                                      :correct  false}]
+                         :range     5})
+           [{:start       10
+             :end         15
+             :correctness 66.7M}])))
 
   (testing "correctness of both 10-20 and 20-30 equations"
-    (is (= (correctness [{:equation    "5*7"
-                          :correct     true}
+    (is (= (correctness {:equations [{:equation "5*7"
+                                      :correct  true}
 
-                         {:equation    "10*12"
-                          :correct     true}
+                                     {:equation "10*12"
+                                      :correct  true}
 
-                         {:equation    "12*13"
-                          :correct     false}])
+                                     {:equation "12*13"
+                                      :correct  false}]
+                         :range     10})
 
            [{:start       0
              :end         10
